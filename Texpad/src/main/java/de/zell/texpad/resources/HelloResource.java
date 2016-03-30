dfdffbc0ecfc6bbd59bf0840e0500927aa9cedad
Zelldon <zelldon91@googlemail.com>
2016-03-30 22:31:57 +0200

    add first resource

---
+++ Texpad/src/main/java/de/zell/texpad/resources/HelloResource.java
@@ -0,0 +1,20 @@
+package de.zell.texpad.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author Christopher Zell <zelldon91@gmail.com>
+ */
+@Path("/Hello")
+public class HelloResource {
+
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+  public String hello() {
+    return "Hello!";
+  }
+}
